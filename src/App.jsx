5d1b3c91b13d88f902c5e157516949b28c90791a
hkothari1 <heetkothari123@gmail.com>
2023-12-30 23:26:50 -0700

    Finished portfolio section and working on contact section

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import Header from './components/header/Header.jsx'
 import Nav from './components/nav/Nav.jsx'
 import About from "./components/about/About.jsx"
 import Experience from "./components/experience/Experience.jsx"
-import Services from "./components/services/Services.jsx"
 import Portfolio from "./components/portfolio/Portfolio.jsx"
-import Testimonials from "./components/testimonials/Testimonials.jsx"
 import Contact from "./components/contact/Contact.jsx"
 import Footer from "./components/footer/Footer.jsx"
 
@@ -18,9 +16,7 @@ const App = () => {
         <Nav></Nav>
         <About></About>
         <Experience></Experience>
-        <Services></Services>
         <Portfolio></Portfolio>
-        <Testimonials></Testimonials>
         <Contact></Contact>
         <Footer></Footer>
 
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './contact.css'
+import { MdOutlineMail } from "react-icons/md";
+import { BiLogoLinkedinSquare } from "react-icons/bi";
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
-  )
+    <section id="contact">
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMail />
+            <h4>Email</h4>
+            <h5>hkothari@ualberta.ca</h5>
+            <a href="mailto:hkothari@ualberta.ca">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BiLogoLinkedinSquare />
+            <h4>LinkedIn</h4>
+            <h5>Heet Kothari</h5>
+            <a href="https://www.linkedin.com/in/heetkothari/">
+              Connect with me
+            </a>
+          </article>
+        </div>
+        <form></form>
+      </div>
+    </section>
+  );
 }
 
 export default Contact
